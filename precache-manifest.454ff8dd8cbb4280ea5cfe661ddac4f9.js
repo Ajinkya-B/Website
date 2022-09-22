da3c03165934e5b30d5bbcf13744fec231ff601e
Ajinkya <ajinkya.bhosale85@gmail.com>
2022-09-22 12:43:04 -0400

    Updates

---
+++ precache-manifest.454ff8dd8cbb4280ea5cfe661ddac4f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a42126f9b25d5e918637",
+    "url": "/website/static/css/main.d47f55e7.chunk.css"
+  },
+  {
+    "revision": "a42126f9b25d5e918637",
+    "url": "/website/static/js/main.a42126f9.chunk.js"
+  },
+  {
+    "revision": "ad0dd6c16c651a926c9d",
+    "url": "/website/static/js/1.ad0dd6c1.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "47ef47ab39af08c83c53461cd3324f2c",
+    "url": "/website/index.html"
+  }
+];
